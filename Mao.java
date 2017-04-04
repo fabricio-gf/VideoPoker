0e6057487a608ea351f06638dd722c629deb1a3a
Edson Y. Toma <edson.toma@usp.br>
2017-04-04 18:30:44 -0300

    Update Mao.java

--- Mao.java
+++ Mao.java
@@ -7,27 +7,25 @@ public class Mao {
    }
 
 public void trocarCartas(String s, Baralho b) throws IllegalArgumentException {
-      Carta troca[];
-	   if (s != null && !s.isEmpty()) {   //se a string estiver vazia presume-se que nenhuma carta sera trocada!
+		Carta troca[];
+		if (s != null && !s.isEmpty()) {   //se a string estiver vazia presume-se que nenhuma carta sera trocada!
 			String[] numeros = s.split("\\s");
 				
 			for (int i = 0; i < numeros.length; i++) {   //as cartas sao representados de 1 para cima
-				if (Integer.parseInt(numeros[i]) - 1 < cartas.length && Integer.parseInt(numeros[i]) > 0) {//entre 0 e n
-					troca = new Carta[numeros.length];
+				if (Integer.parseInt(numeros[i]) - 1 < cartas.length && Integer.parseInt(numeros[i]) > 0) {//entre 0 e length
+					troca = new Carta[1];
 
 					b.trocarCartas(troca);
-                    for(int j = 0; j < troca.length; j++){
-                        cartas[Integer.parseInt(numeros[i]) - 1] = troca[j];
-                        /*
-                        *utiliza cartas[Integer.parseInt(numeros[i])-1]
-                        *porque os valores das posicoes comecam em 1, mas o array em 0
-                        */
-                    }
+					cartas[Integer.parseInt(numeros[i]) - 1] = troca[0];
+/*
+ *utiliza cartas[Integer.parseInt(numeros[i])-1]
+ *porque os valores das posicoes comecam em 1, mas o array em 0
+*/
 				}
-				else throw new IllegalArgumentException("Posicçao inválida!");
+				else throw new IllegalArgumentException("PosicÃ§ao invÃ¡lida!");
 			}
 		}
-   }
+	}
 @Override
 	public String toString(){
 		return "";
