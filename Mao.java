10e4b8d6125d949943e82cc45cd7c21005234202
Edson Y. Toma <edson.toma@usp.br>
2017-04-04 17:42:59 -0300

    Add files via upload

--- Mao.java
+++ Mao.java
@@ -1,27 +1,32 @@
 
 public class Mao {
    private Carta cartas[];
-   private Baralho baralho = new Baralho();
-   
+     
    public Mao(int n) {
 	   cartas = new Carta[n];
    }
    
-   public void gerarMao() throws Exception {
-	   baralho.embaralhar();
-	   cartas = baralho.darCartas(cartas.length);
+  public void setCartas(Carta[] cartas) {
+	  if (cartas.length == this.cartas.length)
+		  this.cartas = cartas;
    }
-   
-   public void trocarCartas(String s) throws IllegalArgumentException {
+
+public void trocarCartas(String s) throws IllegalArgumentException {
 	   if (s != null && !s.isEmpty()) {   //se a string estiver vazia presume-se que nenhuma carta sera trocada!
 			String[] numeros = s.split("\\s");
 				
 			for (int i = 0; i < numeros.length; i++) {   //as cartas sao representados de 1 para cima
-				if (Integer.parseInt(numeros[i]) - 1 < cartas.length && Integer.parseInt(numeros[i]) > 0)
-					cartas[Integer.parseInt(numeros[i])-1] ;//troca a carta da pos   
-				//e as posicoes a partir de de 0, por isso o -1
+				if (Integer.parseInt(numeros[i]) - 1 < cartas.length && Integer.parseInt(numeros[i]) > 0) {//entre 0 e n
+					//fazer a troca
+					//utilizar cartas[Integer.parseInt(numeros[i])-1]
+					//porque os valores das posicoes comecam em 1, mas o array em 0
+				}
 				else throw new IllegalArgumentException("Posicçao inválida!");
 			}
 		}
    }
+@Override
+	public String toString(){
+		return "";
+	}
 }
